4f0c7167645eb91f0018d473da68281cf4c4da14
Buyung Hardiansyah <hardyantz@yahoo.com>
2020-08-15 21:04:22 +0700

    set port

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("Port not set")
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
-
