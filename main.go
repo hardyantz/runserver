27b3a83c9130f6488f32705b898331332eaf2876
Buyung Hardiansyah <hardyantz@yahoo.com>
2020-08-15 20:57:29 +0700

    Revert "set port 80"
    This reverts commit 789062de48d0138f5a7986cd9523b97ca230f478.

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
