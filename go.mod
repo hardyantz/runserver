109bc67220afcf79394ebdcc237c767314ed03b2
Buyung Hardiansyah <hardyantz@yahoo.com>
2020-08-24 18:40:39 +0700

    add slack api

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/slack-go/slack v0.6.6
 	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
+	"github.com/slack-go/slack"
 )
 
 func main() {
@@ -13,9 +15,30 @@ func main() {
 		port = "8009"
 	}
 
+	slackAuth := os.Getenv("SLACK_VERIFICATION_TOKEN")
+	fmt.Println(slackAuth)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/slack/build", PushDeploy)
+
 	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func PushDeploy(c echo.Context) error {
+	s, err := slack.SlashCommandParse(c.Request())
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	if !s.ValidateToken(os.Getenv("SLACK_VERIFICATION_TOKEN")) {
+		return c.String(http.StatusUnauthorized,"invalid token")
+	}
+
+	params := &slack.Msg{Text: s.Text}
+
+	return c.String(http.StatusOK, fmt.Sprintf("deployed branch %v", params.Text))
 }
